5516776fabaa5e1b4c74c8206f4b015db8cb4f08
bag-eunseong <gltdhd@gmail.com>
2024-02-04 16:33:40 +0900

    2

---
+++ src/Day4/BOJ10171.java
@@ -0,0 +1,12 @@
+package Day4;
+
+public class BOJ10171 {
+    public static void main(String[] args) {
+        // 이스케이프!
+        System.out.println(
+                "\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
---
+++ src/Day5/BOJ8393.java
@@ -0,0 +1,18 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class BOJ8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+
+        while(i <= n ){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
